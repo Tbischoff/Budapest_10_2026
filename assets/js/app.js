52b320a04ff2e8c4f4087b4a1c2dcdea247d280f
Tbischoff <tobias.bischoff@web.de>
2026-09-23 13:01:28 +0200

    mobile fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 
-const APP_VERSION = "v1.10.0";
+const APP_VERSION = "v1.10.1";
 
 function syncVersionLabels() {
   document.querySelectorAll(".app-version").forEach(el => { el.textContent = APP_VERSION; });
