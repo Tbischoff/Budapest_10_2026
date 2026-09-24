4fafaa0347a8047179b09b357d07a5476844a334
Tbischoff <tobias.bischoff@web.de>
2026-09-24 17:39:53 +0200

    v1.19.1 Versionsnummer aktualisieren

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 
-const APP_VERSION = "v1.19.0";
+const APP_VERSION = "v1.19.1";
 
 function syncVersionLabels() {
   document.querySelectorAll(".app-version").forEach(el => { el.textContent = APP_VERSION; });
