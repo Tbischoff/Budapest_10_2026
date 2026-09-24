f0c1c8367108977bd68b570c60b41dcd4acbfcd1
Tbischoff <tobias.bischoff@web.de>
2026-09-24 22:00:09 +0200

    v1.22.6 Service-Worker aktualisieren

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "budapest-travel-v1.22.5";
+const CACHE_NAME = "budapest-travel-v1.22.6";
 const OFFLINE_MAP_CACHE = "budapest-offline-map-v1";
-const APP_SHELL = ["./","./index.html","./assets/css/style.css?v=1.22.5","./assets/js/app.js?v=1.22.5","./data/places.js","./assets/icons/favicon.svg","./manifest.webmanifest?v=1.22.5",
+const APP_SHELL = ["./","./index.html","./assets/css/style.css?v=1.22.6","./assets/js/app.js?v=1.22.6","./data/places.js","./assets/icons/favicon.svg","./manifest.webmanifest?v=1.22.6",
   "https://cdn.jsdelivr.net/npm/maplibre-gl@5.7.3/dist/maplibre-gl.css",
   "https://cdn.jsdelivr.net/npm/maplibre-gl@5.7.3/dist/maplibre-gl.js",
   "https://cdn.jsdelivr.net/npm/pmtiles@4.3.0/dist/pmtiles.js",
