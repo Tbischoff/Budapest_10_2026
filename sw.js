6de38469c2583310bdce18cbc697c25c4a2e2339
Tbischoff <tobias.bischoff@web.de>
2026-09-26 00:22:19 +0200

    v1.33.6 Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "budapest-travel-v1.33.5";
+const CACHE_NAME = "budapest-travel-v1.33.6";
 const OFFLINE_MAP_CACHE = "budapest-offline-map-v1";
-const APP_SHELL = ["./","./index.html","./assets/css/style.css?v=1.33.5","./assets/js/app.js?v=1.33.5","./data/places.js","./assets/icons/favicon.svg","./manifest.webmanifest?v=1.33.5",
+const APP_SHELL = ["./","./index.html","./assets/css/style.css?v=1.33.6","./assets/js/app.js?v=1.33.6","./data/places.js","./assets/icons/favicon.svg","./manifest.webmanifest?v=1.33.6",
   "https://cdn.jsdelivr.net/npm/maplibre-gl@5.11.0/dist/maplibre-gl.css",
   "https://cdn.jsdelivr.net/npm/maplibre-gl@5.11.0/dist/maplibre-gl.js",
   "https://cdn.jsdelivr.net/npm/pmtiles@4.3.0/dist/pmtiles.js",
